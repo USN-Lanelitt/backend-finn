70670cd8ec9d12d8aa562e5f1e1c53d98f500aca
altermulig <finn@altermulig.no>
2020-02-04 16:52:13 +0100

    Lagt til tilbakemelding ved login

--- src/Controller/Login.php
+++ src/Controller/Login.php
@@ -6,16 +6,25 @@ use App\Entity\User;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Psr\Log\LoggerInterface;
 
-$request = Request::createFromGlobals();
+//$request = Request::createFromGlobals();
 
 header("Access-Control-Allow-Origin: *");
 
 class Login extends AbstractController
 {
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     public function verifyLogin(Request $request)
     {
-        //$sPAth = $request->getPathInfo();
+        $this->logger->info($request);
+
         $content = json_decode($request->getContent());
         $sUsername = $content->username;
         $sPassword = $content->password;
@@ -23,35 +32,33 @@ class Login extends AbstractController
         //$conn = $this->getEntityManager()->getConnection();
         $conn = $this->getDoctrine()->getConnection();
 
-        $sql = 'SELECT * FROM user p WHERE p.username = :username';
+        // hente ut brukerinfo
+        $sql = 'SELECT * FROM user p WHERE p.username=:username';
         $stmt = $conn->prepare($sql);
         $stmt->execute(['username' => $sUsername]);
 
         // returns an array of arrays (i.e. a raw data set)
-        $return = $stmt->fetchAll();
+        $aReturn['user'] = $stmt->fetchAll();
 
-        $db_iId       = "";
-        $db_sName     = "";
-        $db_sUsername = "";
-        $db_sPassword = "";
-
-        if (count($return) > 0)
+        if (count(aReturn['user']) > 0)
         {
-            $db_iId       = $return[0]['navn'];
-            $db_sName     = $return[0]['navn'];
-            $db_sUsername = $return[0]['username'];
-            $db_sPassword = $return[0]['password'];
+            $db_iIndividId = aReturn['user'][0]['individ_id'];
+            $db_sPassword  = aReturn['user'][0]['password'];
+
+            // hente ut individinfo utifra individ_id fra brukertabellen
+            $sql = 'SELECT * FROM individuals p WHERE p.id=:id';
+            $stmt = $conn->prepare($sql);
+            $stmt->execute(['id' => $db_iIndividId]);
+
+            // returns an array of arrays (i.e. a raw data set)
+            $aReturn['individ'] = $stmt->fetchAll();
         }
 
-        if (password_verify($sPassword, $db_sPassword))
+        if (! password_verify($sPassword, $db_sPassword))
         {
-            $sFeedback = "Bruker ". $db_sUsername ." er logget inn. ID: ".$db_iId." Navn: ".$db_sName;
-        }
-        else
-        {
-            $sFeedback = "Brukernavn eller passord finnes ikke";
+            $aReturn = array();
         }
 
-        return new JsonResponse($sFeedback);
+        return new JsonResponse($aReturn);
     }
 }
--- src/Controller/RegisterUser.php
+++ src/Controller/RegisterUser.php
@@ -2,32 +2,81 @@
 
 namespace App\Controller;
 
+use App\Entity\Individuals;
 use App\Entity\User;
+use App\Entity\Users;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Psr\Log\LoggerInterface;
+use \Datetime;
 
 header("Access-Control-Allow-Origin: *");
 
 class RegisterUser extends AbstractController
 {
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     public function registerUser(Request $request)
     {
+        $this->logger->info($request);
         //$sPAth = $request->getPathInfo();
-        $content = json_decode($request->getContent());
-        $sFullname = $content->name;
-        $sUsername = $content->username;
-        $sPassword = password_hash($content->password, PASSWORD_DEFAULT);
 
-        $user = new User();
-        $user->setNavn($sFullname);
-        $user->setUsername($sUsername);
-        $user->setPassword($sPassword);
+        $content = json_decode($request->getContent());
+        $sFirstname  = $content->firstname;
+        $sMiddlename = ""; //$content->middlename;
+        $sLastname   = $content->lastname;
+        //$sBirthdate  = DateTime::createFromFormat('Y.m.d', $content->birthday);
+        $sBirthdate  = DateTime::createFromFormat('Y-m-d', $content->birthday);
+        $sEmail      = $content->email;
+        $sMobile     = $content->phone;
+        $sPassword   = password_hash($content->password, PASSWORD_DEFAULT);
+
+        //$sFullname = $content->name;
+        //$sUsername = $content->username;
+        //$sBirthdate = DateTime::createFromFormat('d.m.Y', $content->birthday);
+        //$sBirthdate = $content->birthday;
+        //$sBirthdate = DateTime::createFromFormat($sBirthdate, 'Y-m-d');
+        //$sBirthdate = self::convert_date_nor2sql($sBirthdate);
+        //$sBirthdate2 = Datetime::createFromFormat('d.m.Y', $sBirthdate);
+        //$sPassword = password_hash($content->password, PASSWORD_DEFAULT);
+        //$sUsername = $content->username;
+
+        // legge til individ i databasen
+        $oIndividuals = new Individuals();
+        $oIndividuals->setFirstname($sFirstname);
+        $oIndividuals->setMiddlename($sMiddlename);
+        $oIndividuals->setLastname($sLastname);
+        $oIndividuals->setBirthdate($sBirthdate);
+        $oIndividuals->setEmail($sEmail);
+        $oIndividuals->setMobile($sMobile);
 
         $entityManager = $this->getDoctrine()->getManager();
-        $entityManager->persist($user);
+        $entityManager->persist($oIndividuals);
         $entityManager->flush();
 
-        return new JsonResponse("Bruker ble lagret");
+        $iIndividId = $oIndividuals->getId();
+
+        // legge inn bruker
+        $oUsers = new Users();
+        $oUsers->setIndividId($iIndividId);
+        $oUsers->setPassword($sPassword);
+        $oUsers->setRights("1111111111");
+        $oUsers->setUsertype('USER');
+        $oUsers->setActive('T');
+        $oUsers->setNewsSubscription('F');
+        $oUsers->setGdpr('F');
+        $oUsers->setUserterms('F');
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($oUsers);
+        $entityManager->flush();
+
+        return new JsonResponse("Bruker ble opprettet");
     }
 }
